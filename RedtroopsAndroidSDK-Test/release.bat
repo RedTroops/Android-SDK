9ae99bb3f7fa23acb60464b821c9ee9419616fe8
omar_massad <zeroed55@gmail.com>
2014-01-28 08:28:34 +0300

    Initial commit

---
+++ RedtroopsAndroidSDK-Test/release.bat
@@ -0,0 +1,5 @@
+del "bin\classes\com\gpit\android\library\Manifest$permission.class"
+del "bin\classes\com\gpit\android\library\Manifest.class"
+
+del "bin\classes\com\redtroops\RedtroopsAndroidSDK\Manifest$permission.class"
+del "bin\classes\com\redtroops\RedtroopsAndroidSDK\Manifest.class"
