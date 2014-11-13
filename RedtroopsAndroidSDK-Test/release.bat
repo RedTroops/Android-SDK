7086471fdcfd945ef6ec26c98b77146294a4099b
Zaid Daba'een <zaiddabaeen@hotmail.com>
2014-11-13 11:54:29 +0200

    Delete release.bat

--- RedtroopsAndroidSDK-Test/release.bat
+++
@@ -1,5 +0,0 @@
-del "bin\classes\com\gpit\android\library\Manifest$permission.class"
-del "bin\classes\com\gpit\android\library\Manifest.class"
-
-del "bin\classes\com\redtroops\RedtroopsAndroidSDK\Manifest$permission.class"
-del "bin\classes\com\redtroops\RedtroopsAndroidSDK\Manifest.class"
