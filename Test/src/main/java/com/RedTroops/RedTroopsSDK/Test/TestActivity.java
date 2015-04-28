bdd07570621196659048aa8ab368277c255969c2
zaiddabaeen <zaid@redtroops.com>
2015-04-28 10:17:07 +0300

    Test for V3.0.4

--- Test/src/main/java/com/RedTroops/RedTroopsSDK/Test/TestActivity.java
+++ Test/src/main/java/com/RedTroops/RedTroopsSDK/Test/TestActivity.java
@@ -3,22 +3,32 @@ package com.RedTroops.RedTroopsSDK.Test;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
+import com.RedTroops.RedTroopsSDK.AdType;
 import com.RedTroops.RedTroopsSDK.RedTroops;
 import com.RedTroops.RedTroopsSDK.RedTroops.initFinishListener;
 
-public class TestActivity extends Activity {
+public class TestActivity extends FragmentActivity {
+
+	VideoView vv;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test);
 
-		RedTroops.getInstance(this).init(initFinishedListener);
+		getFragmentManager().beginTransaction().replace(R.id.content_frame, new ScrollFragment()).commit();
+
+		RedTroops.getInstance(this).init(initFinishedListener, new AdType().videoAd().bannerAd().audioAd().nativeAd().interstitialAd());
 
 	}
 	
@@ -44,10 +54,26 @@ public class TestActivity extends Activity {
 		}
 	};
 
-	@Override
-	protected void onDestroy() {
-		RedTroops.getInstance(this).endSession();
-		super.onDestroy();
+    public void PlayAudioAdvertisement(View v){
+
+        RedTroops.getInstance(this).playAudioAd(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(TestActivity.this, "Media stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+	public void PlayVideoAdvertisement(View v){
+
+		RedTroops.getInstance(this).playVideoAd(this, new MediaPlayer.OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				Toast.makeText(TestActivity.this, "Media stopped", Toast.LENGTH_SHORT).show();
+			}
+		});
+
 	}
 	
 	@Override
@@ -63,16 +89,13 @@ public class TestActivity extends Activity {
 		super.onResume();
 	}
 
-    public void NextActivity(View v) {
-        Intent i = new Intent(this, SecondActivity.class);
-        startActivity(i);
-    }
-
-	// This is implemented to disable calling onCreate when the screen orientation changes.
-	// Not a part of RedTroops SDK.
 	@Override
-	public void onConfigurationChanged(Configuration newConfig){
-		super.onConfigurationChanged(newConfig);
-	}
+	public void onBackPressed() {
 
+		if(getFragmentManager().getBackStackEntryCount() > 0) {
+			getFragmentManager().popBackStack();
+		} else {
+			super.onBackPressed();
+		}
+	}
 }
